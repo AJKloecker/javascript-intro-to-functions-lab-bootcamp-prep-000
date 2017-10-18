f224d8a6b9d832f6cc2ef035c64bbff9a4502808
Andrew K <ajkloecker@outlook.com>
2017-10-18 16:56:05 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(s){
+  return s.toLowerCase()
+}
+
+function logWhisper(s){
+  console.log(s.toLowerCase())
+}
+
+function sayHiToGrandma(s){
+  if(s = "I love you, Grandma.") return "I love you, too."
+  s == s.toUperCase() ? return "YES INDEED!" : return "I can't hear you!" 
+}
